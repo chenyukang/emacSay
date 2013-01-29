1c1a71b80a8a0b1b3f7693848418a0dd9d9ed8e4
Yukang <moorekang@gmail.com>
2013-01-29 19:25:49 +0800

    rename

--- emacsay-mode.el
+++ emacsay-mode.el
@@ -16,14 +16,14 @@
   (setq cmd_str (buffer-substring low high))
   (start-process  "say" nil "say" cmd_str))
 
-(defun say-current-string()
+(defun emacsay-say-current-string()
   (interactive)
   (let ((pos (point)))
 	(say-this-string)
 	(goto-char pos)))
 
 ;;if last char is not blank, say last word you have just input 
-(defun say-last-string()
+(defun emacsay-say-last-string()
   (interactive)
   (setq input_char (buffer-substring (- (point) 1) (point)))
   (setq  prev_char (buffer-substring (- (point) 2) (- (point) 1)))
@@ -34,12 +34,12 @@
        (say-this-string)
        (goto-char prev_pos))))  
 
-(defun say-buffer()
+(defun emacsay-say-buffer()
   (interactive)
   (setq cmd_str (buffer-substring (point-min) (point-max)))
   (start-process "say" nil "say" cmd_str))
 
-(defun say-stop()
+(defun emacsay-say-stop()
   (interactive)
   (shell-command "pkill -9 say")
   (message "stop saying buffer"))
@@ -48,20 +48,18 @@
   "Keymap for emacsay minor mode")
 (unless emacsay-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "\C-cs" 'say-current-string)
-    (define-key map "\C-cp" 'say-buffer)
-    (define-key map "\C-ct" 'say-stop)
+    (define-key map "\C-cs" 'emacsay-say-current-string)
+    (define-key map "\C-cp" 'emacsay-say-buffer)
+    (define-key map "\C-ct" 'emacsay-say-stop)
     (setq emacsay-mode-map map)))
 
-
-(define-minor-mode emacsay-mode()
+(define-minor-mode emacsay-mode
   "this is the emacsay-mode for Mac OS,
    read the word you have inputed into Emacs"
-  ;; The initial value.
-  :init-value nil
   ;; The indicator for the mode line.
   :lighter " EmacSay"
-  :keymap emacsay-mode-map)
+  :group 'emacsay
+  :keymap emacsay-mode-map
   (add-hook 'post-self-insert-hook 'say-last-string))
   
 (provide 'emacsay-mode)
